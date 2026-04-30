0f47df52603207df41ec66df8e6fc9069e515caa
Mr. Syntax <maam98617@gmail.com>
2026-04-30 16:25:16 +0330

    Update ThemeToggle.jsx

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -13,12 +13,12 @@ function ThemeToggle() {
                rounded-full w-12 h-6  transition-transform duration-300 flex items-center justify-center
                 '>
 
-                {isDark ? <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 18v-1M4.21 4.21l1.42 1.42m12.56 12.56-1.42 1.42M4 12H3m18 0h-1M4.21 19.79l1.42-1.42M19.79 4.21l-1.42 1.42M12 7a5 5 0 100 10 5 5 0 000-10z"></path>
+                {isDark ? <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 18v-1M4.21 4.21l1.42 1.42m12.56 12.56-1.42 1.42M4 12H3m18 0h-1M4.21 19.79l1.42-1.42M19.79 4.21l-1.42 1.42M12 7a5 5 0 100 10 5 5 0 000-10z"></path>
                 </svg>
 
-                    : <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20.354 12.5a9 9 0 11-8.314-7.883A9 9 0 0017.05 12.5h.001z"></path>
+                    : <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M20.354 12.5a9 9 0 11-8.314-7.883A9 9 0 0017.05 12.5h.001z"></path>
                     </svg>
 
                 }
